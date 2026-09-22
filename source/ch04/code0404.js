399f3b89bed380af88e8789ff8991150d98aee13
김경훈 <kkh3363@gmail.com>
2026-09-22 10:53:38 +0900

    Add files via upload

---
+++ source/ch04/code0404.js
@@ -0,0 +1,11 @@
+let array =[10, 20 ,"아침밥", '점심밥', true, false];
+let i = 0;
+
+while ( i < array.length){
+    console.log(array[i]);
+    i++;   
+}
+
+// 증감 값이 일정할 때는 for 문을 작성한다.
+for ( let j=0; j < array.length ; j++ )
+    console.log(array[j]);
